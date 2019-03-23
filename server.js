0738b04e1d6f59d34cf93383b3c77b87615668d3
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-23 13:52:18 -0700

    todo

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// TODO: Cache invalidation for all tables.
+//TODO: add function searchYelp
+//TODO: add function searchTrails
+//TODO: dry query handler: LOTS of ``
+
 //dependencies
 
 //environment variables
@@ -12,12 +17,12 @@ const cors = require('cors');
 const pg = require('pg');
 
 //app setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
 //connection to the client
-const client= new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 //to error is human, to err is machine
@@ -27,29 +32,36 @@ client.on('error', err => console.error(err));
 //location API route
 app.get('/location', searchToLatLong)
 app.get('/weather', searchWeather)
-app.get('/meetups', searchMeetup);
+app.get('/meetups', searchMeetup)
+app.get('/movies', searchMovies);
+app.get('/yelp', searchYelp); 
+
+app.get('/trails', searchTrails);
+
 
 
 //turn the server on so it will listen
-app.listen(PORT, () =>console.log(`listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`City Explorer Backend listening on PORT ${PORT}`));
 
 //error handler - it is called and attached to the function for each route
 function handleError(err, res) {
   console.error(err);
-  if (res) res.status(500).send('⚠︎ So terriably sorry, something has gone very wrong and you should turn back. Now. ⚠');
+  if (res) res.status(500).send('⚠︎ So terriably sorry, something has gone very wrong and you should turn back. Now! ⚠');
 }
 
+
+
 //TEST ROUTE - makes sure server is up
-app.get('/testing', (request, response) =>{
+app.get('/testing', (request, response) => {
   console.log('hit the test route');
-  let testObject = {name: 'test route'}
+  let testObject = { name: 'test route' }
   response.json(testObject);
 })
 
 
 //Helper functions
 
-function searchToLatLong(request, response){
+function searchToLatLong(request, response) {
   let query = request.query.data;
 
   let sql = `SELECT * FROM locations WHERE search_query=$1;`
@@ -57,30 +69,26 @@ function searchToLatLong(request, response){
 
   client.query(sql, values)
     .then(result => {
-      if (result.rowCount > 0){
-        console.log('👹LOCATION FROM SQL');
+      if (result.rowCount > 0) {
         response.send(result.rows[0]);
 
-      }else{
+      } else {
         const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`;
 
         superagent.get(url)
 
-          .then(data =>{
-            console.log('💩LOCATION FROM API');
+          .then(data => {
+            if (!data.body.results.length) { throw 'NO DATA' }
 
-            if(!data.body.results.length){throw 'NO DATA'}
-
-            else{
-              let location = new Location( query, data.body.results[0]);
+            else {
+              let location = new Location(query, data.body.results[0]);
               let newSql = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4) RETURNING id;`;
 
               let newValues = Object.values(location);
-              console.log('💀line', newValues);
               client.query(newSql, newValues)
-              
-                .then( result => {
-                  location.id = result.rows[0].id; 
+
+                .then(result => {
+                  location.id = result.rows[0].id;
 
                   response.send(location);
                 })
@@ -93,7 +101,6 @@ function searchToLatLong(request, response){
 
 //constructor for location. Takes in query and location, accesses it inside the google maps data object and pulls out info
 function Location(query, location) {
-  console.log('👿 line 96', query);
   this.search_query = query;
   this.formatted_query = location.formatted_address;
   this.latitude = location.geometry.location.lat;
@@ -103,7 +110,7 @@ function Location(query, location) {
 //Refactoring weather to use array.maps. Callback function for the /weather path
 //and SQL
 
-function searchWeather(request, response){
+function searchWeather(request, response) {
   let query = request.query.data.id;
 
   let sql = `SELECT * FROM weathers WHERE location_id=$1`
@@ -111,19 +118,18 @@ function searchWeather(request, response){
   let values = [query];
 
   client.query(sql, values)
-    .then(result =>{
-      if(result.rowCount > 0){
+    .then(result => {
+      if (result.rowCount > 0) {
         response.send(result.rows);
-      }else{
+      } else {
         const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-
         superagent.get(url)
           .then(weatherResults => {
-            if(!weatherResults.body.daily.data.length){ throw 'NO DATA'}
-            else{
-              const weatherSummaries = weatherResults.body.daily.data.map(day =>{
+            if (!weatherResults.body.daily.data.length) { throw 'NO DATA' }
+            else {
+              const weatherSummaries = weatherResults.body.daily.data.map(day => {
                 let summary = new Weather(day);
-                summary.id = query; 
+                summary.id = query;
 
                 let newSql = `INSERT INTO weathers (forecast, time, location_id) VALUES($1, $2, $3);`;
                 let newValues = Object.values(summary);
@@ -142,7 +148,7 @@ function searchWeather(request, response){
 }
 
 //constructor for weather. Turns the milliseconds from the original weather data into userfriendly output
-function Weather(day){
+function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
 }
@@ -150,26 +156,81 @@ function Weather(day){
 
 //A function called searchMeetup. Callback function for /meetup path and corresponding constructor function using same structure as search weather function
 //not fully working yet, but we think we're on the right track. Need to figure out what parameters to pass to the group_url to make it access the location
+
 function searchMeetup(request, response) {
-  console.log('You have reached the searchMeetup function')
-  const url = `https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=${request.query.data.longitude}&page=20&lat=${request.query.data.latitude}&key=${process.env.MEETUP_API_KEY}`
-  return superagent.get(url)
-    .then(meetupResults =>{
-      console.log('THIS IS THE NAME OF THE MEETUP', meetupResults.body.events[0].name)
-      const meetupSummaries = meetupResults.body.events.map(daily => {
-        let newMeetup = new Meetup(daily);
-        console.log('THIS IS A NEW MEEETUP', newMeetup)
-        return newMeetup;
-      })
-      response.send(meetupSummaries);
+  let query = request.query.data;
+  let sql = `SELECT * FROM meetups WHERE location_id=$1`
+  let values = [query.id];
+  console.log('VALUES AT !53', values);
+
+  client.query(sql, values)
+    .then(result => {
+      console.log('IMPORTANT IN 157', result);
+      if (result.rowCount > 0) {
+        response.send(result.rows);
+      } else {
+        const url = `https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&lon=${request.query.data.longitude}&page=20&lat=${request.query.data.latitude}&key=${process.env.MEETUP_API_KEY}`
+
+        console.log('THIS IS SUPER IMPORTANT AAAHHHHHH 168');
+        superagent.get(url)
+          .then(meetupResults => {
+            console.log('THIS IS SUPER IMPORTANT AAAHHHHHH 171', meetupResults);
+
+            if (!meetupResults.body.events.length) { throw 'NO DATA' }
+            else {
+              const meetupSummaries = meetupResults.body.events.map(daily => {
+                let newMeetup = new Meetup(daily);
+                newMeetup.id = query.id;
+
+                let newSql = `INSERT INTO meetups (link, name, creation_date, host, location_id) VALUES($1, $2, $3, $4, $5);`;
+                let newValues = Object.values(newMeetup);
+                client.query(newSql, newValues);
+
+                return newMeetup;
+              });
+              response.send(meetupSummaries);
+            }
+          })
+      }
     })
     .catch(error => handleError(error, response));
 }
 
 
-function Meetup(data){
+function Meetup(data) {
   this.link = data.link;
   this.name = data.name;
   this.creation_date = new Date(data.created).toString().slice(0, 15);
   this.host = data.group.name;
+}
+
+//moviedb API
+//searchMovies function
+
+function searchMovies(request, response) {
+  // David
+  let query = request.query.data;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${query.search_query}&page=1&include_adult=false`;
+
+  return superagent.get(url)
+    .then(moviesResults => {
+
+      const movieSummaries = moviesResults.body.results.map(film => {
+        let newMovie = new Movie(film);
+        newMovie.id = query.id;
+        return newMovie;
+      })
+      response.send(movieSummaries);
+    })
+    .catch(error => handleError(error, response));
+}
+
+function Movie(data) {
+  this.title = data.title;
+  this.released_on = data.release_date;
+  this.total_votes = data.vote_count;
+  this.average_votes = data.vote_average;
+  this.popularity = data.popularity;
+  this.image_url = 'https://image.tmdb.org/t/p/w500' + data.poster_path;
+  this.overview = data.overview;
 }
