f2d6c633dafca45e45db50ef5ac191a6e2c772d4
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-24 19:50:39 -0700

    weather refactored for timeout

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 'use strict';
-//TODO: refactor Yelp
-//TODO: refactor trails
 // TODO: Cache invalidation for all tables.
 //TODO: get Yelp image working
 //TODO: split time string for trails
@@ -103,40 +101,83 @@ function Location(query, location) {
   this.longitude = location.geometry.location.lng;
 }
 
+//search function
+
+function getData(sqlInfo){
+  let sql = `SELECT * FROM ${sqlInfo.endpoint}s WHERE location_id=$1;`
+  let values = [sqlInfo.id];
+
+  try { return client.query(sql, values); }
+  catch(error) { handleError(error) }
+}
+
+//cache invalidation
+
+//timeouts object
+const timeouts = {
+  weather: 15 * 1000,
+  yelp: 7*1000*60*60*24,
+  movie: 6*30*1000*60*60*24,
+  meetup: 2*1000*60*60,
+  trail: 24*1000*60*60
+};
+
+//timeouts function
+function checkTimeouts(sqlInfo, sqlData){
+  if(sqlData.rowCount > 0){
+    let resultsAge = (Date.now() - sqlData.rows[0].created_at);
+
+    console.log('endpoint for sql info', sqlInfo.endpoint, 'AGE: ', resultsAge);
+
+    if (resultsAge > timeouts[sqlInfo.endpoint]){
+      let sql = `DELETE FROM ${sqlInfo.endpoint}s WHERE location_id=$1;`;
+      let values = [sqlInfo.id];
+      client.query(sql, values)
+        .then(() => {return null; })
+        .catch(error => handleError(error));
+    }else{ return sqlData }
+  }
+}
+
 //Refactoring weather to use array.maps. Callback function for the /weather path
 //and SQL
 
 function searchWeather(request, response) {
-  let query = request.query.data.id;
-  let sql = `SELECT * FROM weathers WHERE location_id=$1`;
-  let values = [query];
 
-  client
-    .query(sql, values)
+  let sqlInfo = {
+    id: request.query.data.id,
+    endpoint: 'weather',
+  }
+
+  getData(sqlInfo)
+
+    .then(data => checkTimeouts(sqlInfo, data))
     .then(result => {
-      if (result.rowCount > 0) {
-        response.send(result.rows);
-      } else {
+      if (result) {response.send(result.rows)}
+      else {
         const url = `https://api.darksky.net/forecast/${
           process.env.WEATHER_API_KEY
         }/${request.query.data.latitude},${request.query.data.longitude}`;
-        superagent.get(url).then(weatherResults => {
-          if (!weatherResults.body.daily.data.length) {
-            throw 'NO DATA';
-          } else {
-            const weatherSummaries = weatherResults.body.daily.data.map(day => {
-              let summary = new Weather(day);
-              summary.id = query;
-              let newSql = `INSERT INTO weathers (forecast, time, location_id) VALUES($1, $2, $3);`;
-              let newValues = Object.values(summary);
-              client.query(newSql, newValues);
 
-              return summary;
-            });
+        superagent.get(url)
+          .then(weatherResults => {
+            if (!weatherResults.body.daily.data.length) {
+              throw 'NO DATA';
+            } else {
+              const weatherSummaries = weatherResults.body.daily.data.map(day => {
+                let summary = new Weather(day);
+                summary.id = sqlInfo.id;
 
-            response.send(weatherSummaries);
-          }
-        });
+                let newSql = `INSERT INTO weathers (forecast, time, created_at, location_id) VALUES($1, $2, $3, $4);`;
+                let newValues = Object.values(summary);
+                client.query(newSql, newValues);
+
+                return summary;
+              });
+
+              response.send(weatherSummaries);
+            }
+          });
       }
     })
     .catch(error => handleError(error, response));
@@ -146,6 +187,7 @@ function searchWeather(request, response) {
 function Weather(day) {
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toString().slice(0, 15);
+  this.created_at = Date.now();
 }
 
 //A function called searchMeetup. Callback function for /meetup path and corresponding constructor function using same structure as search weather function
@@ -289,12 +331,10 @@ function searchTrails(request, response){
     .query(sql, values)
     .then(result =>{
       if(result.rowCount > 0){
-        console.log('🦇SQL', sql, values)
         response.send(result.rows);
       }else{
         const url = `https://www.hikingproject.com/data/get-trails?lat=${request.query.data.latitude}&lon=${request.query.data.longitude}&maxDistance=10&key=${process.env.HIKING_API_KEY}`;
         superagent.get(url).then(trailsResults => {
-          console.log('🦉API', trailsResults.body)
           if(!trailsResults.body.trails.length){
             throw 'NO DATA';
           }else{
@@ -313,22 +353,6 @@ function searchTrails(request, response){
     })
     .catch(error => handleError(error, response));
 }
-
-// function searchTrails(request, response) {
-//   let query = request.query.data;
-
-//   return superagent
-//     .get(url)
-//     .then(trailsResults => {
-//       const trailSummaries = trailsResults.body.trails.map(hike => {
-//         let newTrail = new Trail(hike);
-//         newTrail.id = query.id;
-//         return newTrail;
-//       });
-//       response.send(trailSummaries);
-//     })
-//     .catch(error => handleError(error, response));
-// }
 
 function Trail(data) {
   this.name = data.name;
